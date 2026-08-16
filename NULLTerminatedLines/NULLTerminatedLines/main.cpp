8d1a5c4dd25985ad0de9d4cfcd6184e6ef7e4ffe
Chesa_mu82@top-academy.ru <Chesa_mu82@top-academy.ru>
2026-08-16 22:40:04 +0500

    added two functions

--- NULLTerminatedLines/NULLTerminatedLines/main.cpp
+++ NULLTerminatedLines/NULLTerminatedLines/main.cpp
@@ -9,6 +9,9 @@ void ToLower(char str[]);                      //переводит строку в нижний регис
 void ShiftToLeft(char str[], int position);    //смещает влево всю строку
 char* Shrink(char str[]);                      //Удаляет лишние пробелы из предложения
 bool Is_palindrome(char str[]);                //Проверяет, является ли строка палиндромом
+bool Is_int_number(char str[]);                //Проверяет, является ли строка целым десятичным числом
+int To_int_number(char str[]);                 //Если строка является целым десятичным числом, возвращает ее числовое значение
+
 
 
 void main()
@@ -31,10 +34,22 @@ void main()
 	Print(str);*/
 
 	//char str[size] = "Аргентина манит негра";
-	char str[size] = {};
+	/*char str[size] = {};
 	cout << "Введите предложение: " ;
 	cin.getline(str, size);
-	cout << "Предложение " << (Is_palindrome(str)? "" : "не") << " палиндром" << endl;
+	cout << "Предложение " << (Is_palindrome(str)? "-" : "не") << " палиндром" << endl;*/
+	
+	/*char str[size] = {};
+	cout << "Введите число или не число: " ;
+	cin.getline(str, size);
+	cout << "Введенная надпись " << (Is_int_number(str)? "-" : "не") << " целое число" << endl;
+	cout << endl;*/
+
+	char str[size] = {};
+	cout << "Введите число: ";
+	cin.getline(str, size);
+	cout << "Введенное число " << To_int_number(str) << endl;
+	cout << endl;
 	main();
 }
 
@@ -93,7 +108,6 @@ char* Shrink(char str[])
 }
 bool Is_palindrome(char str[])
 {
-	bool yeas_no = false;
 	ToUpper(str);
 	for (int i = 0; str[i]; i++)
 	{
@@ -102,17 +116,41 @@ bool Is_palindrome(char str[])
 	int a = StringLength(str);
 	for (int i = 0; i < a; i++, a--)
 	{
-		if (str[i] == str[a - 1])
-		{
-			yeas_no = true;
-			continue;
-		}
-		else
-		{
-			yeas_no = false;
-			break;
-		}
+		if (str[i] != str[a - 1])return false;
 	}
-
+	return true;
+}
+bool Is_int_number(char str[])
+{
+	bool yeas_no = false;
+	for ( int i = 0; str[i]; i++)
+	{
+		if (i == 0 && str[i] == '-') { yeas_no = true; continue; }
+		if (str[i] >= '0' && str[i] <= '9')
+		{
+			yeas_no = true; continue;
+		}
+		else { yeas_no = false; break; }
+	}
 	return yeas_no;
+}
+int To_int_number(char str[])
+{
+	int num = 0;
+	if (Is_int_number(str))
+	{
+		for (int i = 0;  str[i]; i++)
+		{
+			if (i == 0 && str[0] == '-')continue;
+			if (i == 1 && str[0] == '-')
+			{
+				num = -(str[i] - '0'); continue;
+			}
+			if (str[0] == '-')
+			num = num * 10 - (str[i] -'0');
+			else
+			num = num * 10 + (str[i] -'0');
+		}
+	};
+	return num;
 }
